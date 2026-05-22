536da37d51cc68d932baf2648efbbfe7f15aadf8
Oskar Kulinski <oskarkulinski3@gmail.com>
2026-05-22 23:08:51 +0200

    Updated transformer script to use the same epochs, env steps and other hyperparams as the original paper

--- job-transformers.sh
+++ job-transformers.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #
-#SBATCH --job-name=transformer-small
+#SBATCH --job-name=transformer-crl
 #SBATCH --partition=common
 #SBATCH --qos=ok479034_common
 #SBATCH --gres=gpu:1
 #SBATCH --output=logs/slurm-transformer-small.txt
 #SBATCH --error=logs/slurm-transformer-small-error.txt
-#SBATCH --time=30
+#SBATCH --time=16:00:00
 
 echo "Started"
 
@@ -15,8 +15,9 @@ set -eux
 uv run main.py \
   --env_id "ant" \
   --eval_env_id "ant" \
-  --num_epochs 10 \
-  --total_env_steps 300_000 \
+  --num_epochs 100 \
+  --episode_length 1000 \
+  --total_env_steps 100_000_000 \
   --critic_depth 16 \
   --actor_depth 16 \
   --actor_skip_connections 4 \
@@ -24,8 +25,8 @@ uv run main.py \
   --vis_length 1000  \
   --save_buffer 0  \
   --num_envs 16 \
-  --min_replay_size 2000 \
-  --unroll_length 20 \
+  --min_replay_size 1000 \
+  --max_replay_size 10_000 \
   --use_transformer  1 \
   --transformer_embed_dim 144\
   --transformer_num_layers 4 \
